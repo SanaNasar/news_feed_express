bb8ec577eb5da90ac0b5884caa7869d636f6f713
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-21 22:57:39 -0700

    Including the main app.js file

--- app.js
+++ app.js
@@ -8,12 +8,32 @@ app.use(bodyParser.urlencoded());
 
 app.set('view engine', 'ejs');
 
+var articles = [];
 
 
-// app.get('/articles', function(req, res){
-// 	res.redirect('/articles');
-	 
-// });
+ 
+//Added the static pages
+// Routes
+
+app.get('/articles', function(req, res) {
+	res.render('articles/articles', { articles: articles });
+});
+
+app.get('/articles/:id', function(req, res) {
+	var index = req.params.id;
+	var article = articles[index];
+	res.render('articles/show', { article: article });
+});
+
+app.get('/articles/new', function(req, res) {
+	res.render('articles/new');
+});
+
+app.post('/articles', function(req, res) {
+	articles.push(req.body.article);
+	console.log(req.body.article);
+	res.redirect('/articles');
+});
 
 app.get('/contact', function(req, res){
   // use a render
